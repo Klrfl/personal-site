334900672f7de02efe8002870cff100cc1a06b72
Efraim Munthe <efrayanglain@gmail.com>
2023-06-25 16:17:54 +0700

    changed nav styling and selectors

--- src/scripts/navbar.js
+++ src/scripts/navbar.js
@@ -1,4 +1,4 @@
-const navlinks = document.querySelector(".nav-links");
+const navlinks = document.querySelector(".nav-links-wrapper");
 const openNavbarBtn = document.getElementById("open-nav");
 const closeNavbarBtn = document.getElementById("close-nav");
 
