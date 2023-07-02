d4e2e3711be1768d2d798de8dedfbcf8a2ec6b6e
Efraim Munthe <efrayanglain@gmail.com>
2023-07-02 18:29:58 +0700

    add date formatting

--- src/scripts/utils.js
+++ src/scripts/utils.js
@@ -3,3 +3,16 @@ export function sortPostsByDate(posts) {
     return new Date(b.frontmatter.pubDate) - new Date(a.frontmatter.pubDate);
   });
 }
+
+export function formatDate(
+  inputDate,
+  options = {
+    weekDay: "long",
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  }
+) {
+  const date = new Date(inputDate);
+  return Intl.DateTimeFormat(undefined, options).format(date);
+}
