a27fbf6d7f669f44c0bad85f4931e40f376bd919
Efraim Munthe <efrayanglain@gmail.com>
2023-08-31 11:35:35 +0700

    show sections on scroll

---
+++ src/scripts/hero.js
@@ -0,0 +1,24 @@
+const observerOptions = {
+  rootMargin: "10px",
+  threshold: 0.2,
+};
+
+const mainSectionObserver = new IntersectionObserver(
+  (entries, mainSectionObserver) => {
+    entries.forEach((entry) => {
+      if (entry.isIntersecting) {
+        entry.target.classList.remove("hidden");
+      }
+    });
+  },
+  observerOptions
+);
+
+const mainSections = document.querySelectorAll("section.main-section");
+
+document.addEventListener("DOMContentLoaded", () => {
+  mainSections.forEach((mainSection) => {
+    mainSection.classList.add("hidden");
+    mainSectionObserver.observe(mainSection);
+  });
+});
