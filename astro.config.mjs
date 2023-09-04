4dfd0925300df945267f5edd88a9e708ed90d2c3
Efraim Munthe <efrayanglain@gmail.com>
2023-09-04 14:44:07 +0700

    migrate to astro v3, removed old Image imports

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,10 +1,9 @@
 import { defineConfig } from "astro/config";
-import image from "@astrojs/image";
 
 import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://klrfl.netlify.app",
-  integrations: [image(), sitemap()],
+  integrations: [sitemap()],
 });
