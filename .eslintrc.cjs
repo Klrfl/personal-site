12d3f65072b3393ddda624cb84929a11bfa23c84
Efraim Munthe <efrayanglain@gmail.com>
2024-01-02 19:48:34 +0700

    add eslint plugin for astro

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -3,13 +3,14 @@ module.exports = {
     browser: true,
     es2021: true,
   },
-  extends: "eslint:recommended",
+  extends: ["eslint:recommended", "plugin:astro/recommended"],
   overrides: [
     {
       env: {
         node: true,
       },
-      files: [".eslintrc.{js,cjs}"],
+      files: [".eslintrc.{js,cjs}", "*.astro"],
+      parser: "astro-eslint-parser",
       parserOptions: {
         sourceType: "script",
       },
