4d8f438bcc13ca43b1b11f581a93953f6dd82d0a
Efraim Munthe <efrayanglain@gmail.com>
2024-04-07 12:33:25 +0700

    add fonts from fontsource and astro-icon

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,9 +1,10 @@
 import { defineConfig } from "astro/config";
-
 import sitemap from "@astrojs/sitemap";
+
+import icon from "astro-icon";
 
 // https://astro.build/config
 export default defineConfig({
   site: "https://klrfl.netlify.app",
-  integrations: [sitemap()],
+  integrations: [sitemap(), icon()]
 });
