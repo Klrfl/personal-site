f270cf59e876512a937d890bb020bd0c77910478
Efraim Munthe <efrayanglain@gmail.com>
2024-07-11 21:00:51 +0700

    integrate uploadcare (not tested)

--- src/scripts/cms.js
+++ src/scripts/cms.js
@@ -1,5 +1,7 @@
 import CMS from "decap-cms-app";
 import styles from "../styles/preview.css";
+import uploadcare from "decap-cms-media-library-uploadcare";
 
 CMS.init();
+CMS.registerMediaLibrary(uploadcare);
 CMS.registerPreviewStyle(styles.toString(), { raw: true });
