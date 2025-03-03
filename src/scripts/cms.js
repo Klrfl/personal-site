11f02625f9f98d0ba01ac122110625ebc58197a5
Efraim Munthe <efrayanglain@gmail.com>
2025-03-03 12:38:09 +0700

    fix: build error caused by cms style

--- src/scripts/cms.js
+++ src/scripts/cms.js
@@ -1,7 +1,21 @@
 import CMS from "decap-cms-app";
-import styles from "../styles/preview.css";
 import uploadcare from "decap-cms-media-library-uploadcare";
 
 CMS.init();
 CMS.registerMediaLibrary(uploadcare);
-CMS.registerPreviewStyle(styles.toString(), { raw: true });
+CMS.registerPreviewStyle(
+  `
+.frame-content {
+  max-width: 60rem;
+  margin-inline: auto;
+}
+
+.frame-content img {
+  display: block;
+  max-width: 100%;
+  margin-inline: auto;
+  margin-block: 1rem;
+}
+`,
+  { raw: true },
+);
