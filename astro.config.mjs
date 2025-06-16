1c3be85baaa624f3cfd8ebc483995c9ee349303f
Efraim Munthe <efrayanglain@gmail.com>
2025-06-16 08:14:48 +0700

    feat: add astro mdx

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,10 +3,12 @@ import sitemap from "@astrojs/sitemap";
 
 import icon from "astro-icon";
 
+import mdx from "@astrojs/mdx";
+
 // https://astro.build/config
 export default defineConfig({
   site: "https://munthe.dev",
-  integrations: [sitemap(), icon()],
+  integrations: [sitemap(), icon(), mdx()],
   i18n: {
     locales: ["en", "id"],
     defaultLocale: "en",
